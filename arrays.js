ad64bcea4321717af7fdab8a05c3964f509c22f3
Sara Connelly <sara.k.connelly@gmail.com>
2023-06-07 14:46:47 -0600

    Add index position explanation

--- arrays.js
+++ arrays.js
@@ -3,7 +3,7 @@ var calories = [100, 550, 250, 18];
 var rottenFoods = [true, false, false, true];
 
 console.log(foods.slice(1,3))
-// The `.slice` method outputs elements from element[1] to element[3]. 
+// The `.slice` method outputs elements from index position[1] to index position[3]. 
 // `.slice` includes its starting point [1], but not its ending point [3].
 
 console.log(calories.push())
@@ -12,3 +12,7 @@ console.log(calories.push())
 
 console.log(rottenFoods.shift())
 // `.shift` outputs the first element in our array. 
+
+// Index positions are the numerical assignments to each element, based on their order in the array. 
+// We start counting from 0, so the first element has an index position of 0. 
+// The fourth element would have an index position of 3. 
