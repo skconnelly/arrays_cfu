b59a44a90657d5bd292a63f22d14fba1a15af7a4
Sara Connelly <sara.k.connelly@gmail.com>
2023-06-07 14:43:01 -0600

    Add 3 array methods and comments

--- arrays.js
+++ arrays.js
@@ -1,3 +1,14 @@
 var foods = ["burger", "pizza", "kale", "pasta"];
 var calories = [100, 550, 250, 18];
 var rottenFoods = [true, false, false, true];
+
+console.log(foods.slice(1,3))
+// The `.slice` method outputs elements from element[1] to element[3]. 
+// `.slice` includes its starting point [1], but not its ending point [3].
+
+console.log(calories.push())
+// The `.push` method appends (or adds) values to an array. 
+// In this case, that means `.push` tells us that there are 4 elements. 
+
+console.log(rottenFoods.shift())
+// `.shift` outputs the first element in our array. 
